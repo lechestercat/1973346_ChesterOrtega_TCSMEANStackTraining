6f24ff0ad4fde499fc9587ae6b02c64fa05426bc
lechestercat <chesterortega1991@gmail.com>
2021-03-16 18:16:59 -0400

    Need to display blogs

--- simply-blogging/js/script.js
+++ simply-blogging/js/script.js
@@ -12,6 +12,7 @@ function addBlog(){
     blogData.push(formData);
     console.log(blogData);
     storeInLocal();
+    displayBlog();
   
 }
 
@@ -28,4 +29,24 @@ function resetForm() {
     document.getElementById("desc").value="";  
     document.getElementById("img").value="";
 
+}
+
+
+function displayBlog(){
+    var retrievedObject =JSON.parse(localStorage.getItem("blogData"));
+    var blogs1 = '';
+    for (blogpost of blogpost1) {
+    blogs1 += '<div>' +
+    '<div class="card border-secondary mb-3">' +
+    '<h5 class="card-header"></p></h5>' +
+    '<div class="card-body">' +
+    '<div class="blog-post">' +
+    '<h2 class="blog-post-title"></h2><br>' +
+    '</p>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div><br></br>'
+    }
+    document.getElementById("blogs").innerHTML = blogs1;
 }
