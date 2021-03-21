28d0ff61433c55f2ee07488eddd6e9b335ab837f
lechestercat <chesterortega1991@gmail.com>
2021-03-21 16:15:41 -0400

    Added ability to add items to cart

---
+++ cart-management/ts/cart.js
@@ -0,0 +1,108 @@
+// initalie empty cart
+var cart = [];
+// class of type products
+var Product = /** @class */ (function () {
+    function Product(id, name, price, qty) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+    }
+    return Product;
+}());
+// creat products for page
+var laptop = new Product(1, "Pear Laptop Pro", 1000, 1);
+var phone = new Product(2, "Pear Phone Max", 500, 1);
+var couch = new Product(3, "Couch", 700, 1);
+var desk = new Product(4, "Desk", 200, 1);
+var tree = new Product(5, "Tree", 100, 1);
+var bottle = new Product(6, "Water Bottle", 5, 1);
+// add products to cart
+// each onclick first checks if item is in cart just to update quantity
+// else add item to cart
+window.onload = function () {
+    var addLaptop = document.getElementById("laptop");
+    addLaptop.onclick = function () {
+        if (cart.some(function (x) { return x === laptop; })) {
+            var index = cart.indexOf(laptop);
+            cart[index].qty += 1;
+        }
+        else {
+            {
+                cart.push(laptop);
+            }
+        }
+        console.log("LAPTOP ADDED!");
+        console.log(cart);
+    };
+    var addPhone = document.getElementById("phone");
+    addPhone.onclick = function () {
+        if (cart.some(function (x) { return x === phone; })) {
+            var index = cart.indexOf(phone);
+            cart[index].qty += 1;
+        }
+        else {
+            {
+                cart.push(phone);
+            }
+        }
+        console.log("PHONE ADDED!");
+        console.log(cart);
+    };
+    var addCouch = document.getElementById("couch");
+    addCouch.onclick = function () {
+        if (cart.some(function (x) { return x === couch; })) {
+            var index = cart.indexOf(couch);
+            cart[index].qty += 1;
+        }
+        else {
+            {
+                cart.push(couch);
+            }
+        }
+        console.log("COUCH ADDED!");
+        console.log(cart);
+    };
+    var addDesk = document.getElementById("desk");
+    addDesk.onclick = function () {
+        if (cart.some(function (x) { return x === desk; })) {
+            var index = cart.indexOf(desk);
+            cart[index].qty += 1;
+        }
+        else {
+            {
+                cart.push(desk);
+            }
+        }
+        console.log("DESK ADDED!");
+        console.log(cart);
+    };
+    var addTree = document.getElementById("tree");
+    addTree.onclick = function () {
+        if (cart.some(function (x) { return x === tree; })) {
+            var index = cart.indexOf(tree);
+            cart[index].qty += 1;
+        }
+        else {
+            {
+                cart.push(tree);
+            }
+        }
+        console.log("TREE ADDED!");
+        console.log(cart);
+    };
+    var addBottle = document.getElementById("bottle");
+    addBottle.onclick = function () {
+        if (cart.some(function (x) { return x === bottle; })) {
+            var index = cart.indexOf(bottle);
+            cart[index].qty += 1;
+        }
+        else {
+            {
+                cart.push(bottle);
+            }
+        }
+        console.log("WATERBOTTLE ADDED!");
+        console.log(cart);
+    };
+};
---
+++ cart-management/ts/cart.ts
@@ -0,0 +1,104 @@
+// initalie empty cart
+var cart = [];
+
+// class of type products
+class Product {
+    id:number;
+    name:string;
+    price:number;
+    qty:number;
+
+    constructor(id:number, name:string, price:number, qty: number) {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+    }
+    
+}
+
+// creat products for page
+let laptop = new Product(1, "Pear Laptop Pro", 1000, 1);
+let phone = new Product(2, "Pear Phone Max", 500, 1);
+let couch = new Product(3, "Couch", 700, 1);
+let desk = new Product(4, "Desk", 200, 1);
+let tree = new Product(5, "Tree", 100, 1);
+let bottle = new Product(6, "Water Bottle", 5, 1);
+
+
+// add products to cart
+// each onclick first checks if item is in cart just to update quantity
+// else add item to cart
+window.onload = () => {
+    var addLaptop = document.getElementById("laptop");
+    addLaptop.onclick = () => {
+        if(cart.some(x => x === laptop)){
+            let index = cart.indexOf(laptop);
+            cart[index].qty += 1;
+        }else{{
+            cart.push(laptop);
+        }}
+        
+        console.log("LAPTOP ADDED!");
+        console.log(cart);
+    }
+    var addPhone = document.getElementById("phone");
+    addPhone.onclick = () => {
+        if(cart.some(x => x === phone)){
+            let index = cart.indexOf(phone);
+            cart[index].qty += 1;
+        }else{{
+            cart.push(phone);
+        }}
+
+        console.log("PHONE ADDED!");
+        console.log(cart);
+    }
+    var addCouch = document.getElementById("couch");
+    addCouch.onclick = () => {
+        if(cart.some(x => x === couch)){
+            let index = cart.indexOf(couch);
+            cart[index].qty += 1;
+        }else{{
+            cart.push(couch);
+        }}
+        console.log("COUCH ADDED!");
+        console.log(cart);
+    }
+    var addDesk = document.getElementById("desk");
+    addDesk.onclick = () => {
+        if(cart.some(x => x === desk)){
+            let index = cart.indexOf(desk);
+            cart[index].qty += 1;
+        }else{{
+            cart.push(desk);
+        }}
+        console.log("DESK ADDED!");
+        console.log(cart);
+    }
+    var addTree = document.getElementById("tree");
+    addTree.onclick = () => {
+        if(cart.some(x => x === tree)){
+            let index = cart.indexOf(tree);
+            cart[index].qty += 1;
+        }else{{
+            cart.push(tree);
+        }}
+        console.log("TREE ADDED!");
+        console.log(cart);
+    }
+    var addBottle = document.getElementById("bottle");
+    addBottle.onclick = () => {
+        if(cart.some(x => x === bottle)){
+            let index = cart.indexOf(bottle);
+            cart[index].qty += 1;
+        }else{{
+            cart.push(bottle);
+        }}
+        console.log("WATERBOTTLE ADDED!");
+        console.log(cart);
+    }
+    
+
+}
+
