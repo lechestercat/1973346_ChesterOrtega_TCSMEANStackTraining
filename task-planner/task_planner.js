780f1c06fd79d8b93d244e75bc9e17fbc3a2b9f1
lechestercat <chesterortega1991@gmail.com>
2021-04-09 17:21:23 -0400

    Finished all tasks and works 100% MIGHT add styling but doubt it

--- task-planner/task_planner.js
+++ task-planner/task_planner.js
@@ -8,21 +8,21 @@ let server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html',
   });
-  console.log(req.url);
+
   if (req.url != '/favicon.ico') {
-    //html form
+    //html forms
     res.write(`
         <h2>Add Task</h2>
-        <form action="store" method="get">
+        <form>
           <label>Emp ID: </label>
           <input type="text" name="empId"/><br/>
           <label>Task ID: </label>
           <input type="text" name="taskId"/><br/>
           <label>Task: </label>
           <input type="text" name="task"/><br/>
-          <label>Deadliune: </label>
+          <label>Deadline: </label>
           <input type="date" name="deadline"/><br/>
-          <input type="submit" value="Add Task"/>
+          <button type="submit" value="Add" formaction="/store" method="get">Add Task</button>
           <input type="reset" value="Reset"/>
         </form>
 
@@ -32,47 +32,113 @@ let server = http.createServer((req, res) => {
         <form>
             <label>Task ID: </label>
             <input type="text" name="taskId"/><br/>
-            <input type="submit" value="Delete Task"/>
+            <button type="submit" value="Delete" formaction="/delete" method="get">Delete Task</button>
             <input type="reset" value="Reset"/>
         </form>
 
         <br />
         <h2>List Task</h2>
         <form>
-            <input type="submit" onclick="" value="List All Tasks"/>
+        <button type="submit" value="Display" formaction="/display">Display Tasks</button>
         </form>
 
 `);
-
+    //stores data
     if (req.url.includes('/store')) {
-      console.log(url.parse(req.url, true));
-      let data = url.parse(req.url, true).query;
-      console.log('EmpID: ' + data.empId);
       // take the value from url
-      // convert to object
-      // store records in object using push method
+      let data = url.parse(req.url, true).query;
+
+      // convert to object and save params
+      let task = {};
+      task.empId = data.empId;
+      task.taskId = data.taskId;
+      task.task = data.task;
+      task.deadline = data.deadline;
+      console.log('task:');
+      console.log(task);
+
+      // read from json file
+      let tasksStr = fs.readFileSync('tasks.json');
+      // parse the string
+      let tasks = JSON.parse(tasksStr);
+      console.log('tasks:');
+      console.log(tasks);
+
+      //store using push method and checks for duplicates
+      let dup = false;
+      tasks.forEach(function (e) {
+        //checks for duplicates
+        if (task.taskId === e.taskId) {
+          dup = true;
+        }
+      });
+      // pushes if unique
+      if (!dup) {
+        tasks.push(task);
+      } else {
+        //logs error when dup found
+        console.log('Duplicate task ID!');
+      }
       //converet to string
+      let taskStr = JSON.stringify(tasks);
+      console.log(taskStr);
       // store using fs module.
-    } else if (req.url == '/delete') {
+      fs.writeFileSync('tasks.json', taskStr);
+
+      // delete task function
+    } else if (req.url.includes('/delete')) {
+      // grab params
+      let data = url.parse(req.url, true).query;
+
       // read from file
+      let tasksStr = fs.readFileSync('tasks.json');
+
       // convert to json
+      let tasks = JSON.parse(tasksStr);
+
       // check value using iterator or loop
-      // delete using array method's
-      // store in file using fs module.
-      //if task id not available display error message.
-    } else if (req.url == '/display') {
+      let i = 0;
+      tasks.forEach(function (e) {
+        //if task is found delete
+        if (data.taskId === e.taskId) {
+          // removes that tasks and logs the message
+          tasks.splice(i, 1);
+          console.log('task deleted');
+          return;
+        }
+        i++;
+      });
+
+      // store in file using fs module after stringify
+      tasksStr = JSON.stringify(tasks);
+      fs.writeFileSync('tasks.json', tasksStr);
+    } else if (req.url.includes('/display')) {
       // read from file
-      // convert to json
-      //iterator loop
-      // create tableData variable using backticks
-      /*
-                    <table>
-                    <tr>
-                            <td>${variableName}</td>
-                    </tr>
-                    </table>
-                    res.end(tableDatavariable);
-                */
+      let taskString = fs.readFileSync('tasks.json');
+
+      // convert to JSON
+      let taskJson = JSON.parse(taskString);
+
+      // diplay table of tasks
+      let table = `
+            <table style="border:1px solid black;">
+                <thead><tr>
+                    <th style="border:1px solid black;">Emp ID</th>
+                    <th style="border:1px solid black;">Task ID</th>
+                    <th style="border:1px solid black;">Description</th>
+                    <th style="border:1px solid black;">Deadline</th>
+                </tr></thead>`;
+      taskJson.forEach(function (e) {
+        table += `
+                <tr>
+                    <td style="border:1px solid black;">${e.empId}</td>
+                    <td style="border:1px solid black;">${e.taskId}</td>
+                    <td style="border:1px solid black;">${e.task}</td>
+                    <td style="border:1px solid black;">${e.deadline}</td>
+                </tr>`;
+      });
+      table += `</table>`;
+      res.end(table);
     }
   }
 });
